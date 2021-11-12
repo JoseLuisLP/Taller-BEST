5ea62e39bdfa35034b72b253e7503899a1456c89
JOSE LUIS <joseluislaurapumaleque@gmail.com>
2021-11-12 11:54:20 -0500

    Initial commit

---
+++ Taller-MD/Web Scraping/PautasXaringan.R
@@ -0,0 +1,11 @@
+# Algunas pautas para Xaringan
+
+#Usar temas que ya existe
+library(xaringan)
+
+names(xaringan:::list_css())
+highlightLines: true
+countIncrementalSlides: false
+fig.caption: yes
+number_sections: yes
+toc: yes
